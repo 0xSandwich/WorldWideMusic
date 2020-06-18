a9516a405f076475156f0f7e485278864a3b4626
CathyDolle <cathy.dolle@live.fr>
2020-06-19 01:57:09 +0200

    home page done :sparkles:

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import logo from "../assets/images/logo.svg";
+import React from "react"
+import { NavLink } from "react-router-dom"
+import logo from "../assets/images/logo.svg"
 
 function Header() {
-    return (
-        <header>
-            <img id="header-logo" src={logo} alt="header-logo"/>
-            <nav>
-                <ul>
-                    <li><NavLink to="/home">Accueil</NavLink></li>
-                    <li><NavLink to="/map">Map</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <img id="header-logo" src={logo} alt="header-logo" />
+      <nav>
+        <ul>
+          <li>
+            <NavLink className="link-active" to="/home">home</NavLink>
+            <span>|</span>
+          </li>
+          <li>
+            <NavLink to="/map">Map</NavLink>
+            <span>|</span>
+          </li>
+          <li>
+            <NavLink to="/map">About</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react"
+import wave from "../assets/images/waveFixe.svg"
+import map from "../assets/images/map.svg"
+import { Link } from "react-router-dom"
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <section className="home-container">
+      <h1>Welcome to worldwide music</h1>
+      <p>
+        World Wide Music is here to allow you to visualize the concrete
+        production of different musical genres in time and space to show their
+        impact and evolution trough our interactive map ! Let yourself be
+        immersed in the magical world of music.
+      </p>
+
+      <Link to="/map">
+        <div className="start-button">start</div>
+      </Link>
+      <img className="map" src={map}></img>  
+      <img className="wave" src={wave}></img>
+    </section>
+  )
 }
 
 export default Home
