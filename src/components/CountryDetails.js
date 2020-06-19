7b604dff9e5758dbd4235dff04c671cb5e55a46f
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-06-19 06:43:19 +0200

    Modal percentage calc and display

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,32 +1,57 @@
 import React from 'react'
 import genderColors from "../assets/GenderColors";
+import { map } from 'jquery';
 
 function CountryDetails(props) {
     let colors = Object.entries(genderColors)
     let country = (props.data == null) ? '1' : props.data[0].country
     let decade = (props.data == null) ? '1' : props.data[0].decade
-    let genres = (props.data == null) ? '1' : props.data[0]
+    let genresName = ['Blues','Classical','Electronic','Folk',
+    'Funk','Hip-Hop','Jazz','Latin','Pop','Reggae','Rock']
+    let genres = []
+    let percentage = []
+    let nbrAlbums
 
-    let data = []
-    if(props.data != null){
-        colors.map((element,index)=>{
-            data.push(
-            [
-                props.data[0].blues,
-                props.data[0].classical,
-                props.data[0].electronic,
-                props.data[0].folk,
-                props.data[0].funk,
-                props.data[0].hiphop,
-                props.data[0].jazz,
-                props.data[0].latin,
-                props.data[0].pop,
-                props.data[0].reggae,
-                props.data[0].rock
-            ])
-        })
+    if(props.data != null)
+    {
+        // Create array of genres
+        genres.push(
+            props.data[0].blues,
+            props.data[0].classical,
+            props.data[0].electronic,
+            props.data[0].folk,
+            props.data[0].funk,
+            props.data[0].hiphop,
+            props.data[0].jazz,
+            props.data[0].latin,
+            props.data[0].pop,
+            props.data[0].reggae,
+            props.data[0].rock
+        )
+        // Albums number
+        nbrAlbums = genres.reduce((pv, cv) => pv + cv, 0);
+
+        // Create array and calc percentage of all styles
+        genres.map((element, key) =>
+            percentage.push((element / nbrAlbums) * 100)
+        )
     }
-    console.log(data[0]);
+
+    // console.log(data[0]);
+    console.log(nbrAlbums)
+
+
+    let tablePrint = colors.map((element, key) =>
+        <>
+        <tr>
+            <th scope="row" key={key + Math.random()}>Couleur</th>
+            <th scope="row" key={key + Math.random()}>{genresName[key]}</th>
+            <th scope="row" key={key + Math.random()}>{genres[key]}</th>
+            <th scope="row" key={key + Math.random()}>{percentage[key]}</th>
+            <th scope="row" key={key + Math.random()}>Couleur</th>
+        </tr>
+        </>
+    );
     
 
     return (
@@ -35,6 +60,7 @@ function CountryDetails(props) {
                 <h1 hidden={country ? false : true} style={{color:"white"}}>{country} in {decade}</h1>
             </div>
             <div className="country-modal">
+                <i className="close-modal">Close this shit</i>
                 <h1>MOST PRODUCED GENRES</h1>
                 <h3>Number of produced albums</h3>
                 <h3>Most produced genres</h3>
@@ -43,13 +69,9 @@ function CountryDetails(props) {
                     <th scope="col"></th>
                     <th scope="col"></th>
                     <th scope="col"></th>
+                    <th scope="col"></th>
                 </tr>
-                <tr>
-                    <th scope="row"></th>
-                    <th scope="row"></th>
-                    <th scope="row"></th>
-                    <th scope="row"></th>
-                </tr>
+                {tablePrint}
             </div>
         </>
     )
