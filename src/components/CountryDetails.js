42fc6b4f2b046cedfdd8109fb1e504e501f26ba5
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-06-19 05:55:27 +0200

    Meilleur code de la décénnie

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,23 +1,57 @@
 import React from 'react'
+import genderColors from "../assets/GenderColors";
 
 function CountryDetails(props) {
-    let country = (props.data == null) ? null : props.data[0].country
-    let decade = (props.data == null) ? null : props.data[0].decade
-    let blues = (props.data == null) ? null : props.data[0].blues
-    let classical = (props.data == null) ? null : props.data[0].classical
-    let electronic = (props.data == null) ? null : props.data[0].electronic
-    let folk = (props.data == null) ? null : props.data[0].folk
-    let funk = (props.data == null) ? null : props.data[0].funk
-    let hiphop = (props.data == null) ? null : props.data[0].hiphop
-    let jazz = (props.data == null) ? null : props.data[0].jazz
-    let latin = (props.data == null) ? null : props.data[0].latin
-    let pop = (props.data == null) ? null : props.data[0].pop
-    let reggae = (props.data == null) ? null : props.data[0].reggae
-    let rock = (props.data == null) ? null : props.data[0].rock 
+    let colors = Object.entries(genderColors)
+    let country = (props.data == null) ? '1' : props.data[0].country
+    let decade = (props.data == null) ? '1' : props.data[0].decade
+    let genres = (props.data == null) ? '1' : props.data[0]
+
+    let data = []
+    if(props.data != null){
+        colors.map((element,index)=>{
+            data.push(
+            [
+                props.data[0].blues,
+                props.data[0].classical,
+                props.data[0].electronic,
+                props.data[0].folk,
+                props.data[0].funk,
+                props.data[0].hiphop,
+                props.data[0].jazz,
+                props.data[0].latin,
+                props.data[0].pop,
+                props.data[0].reggae,
+                props.data[0].rock
+            ])
+        })
+    }
+    console.log(data[0]);
+    
+
     return (
-        <div className="map-breadcrumb">
-            <h1 hidden={country ? false : true} style={{color:"white"}}>{country} in {decade}</h1>
-        </div>
+        <>
+            <div className="map-breadcrumb">
+                <h1 hidden={country ? false : true} style={{color:"white"}}>{country} in {decade}</h1>
+            </div>
+            <div className="country-modal">
+                <h1>MOST PRODUCED GENRES</h1>
+                <h3>Number of produced albums</h3>
+                <h3>Most produced genres</h3>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+                <tr>
+                    <th scope="row"></th>
+                    <th scope="row"></th>
+                    <th scope="row"></th>
+                    <th scope="row"></th>
+                </tr>
+            </div>
+        </>
     )
 }
 
