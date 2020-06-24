cea7db4f1ff26d0c47a582574f61972e48f16b80
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-06-24 21:10:26 +0200

    Fix security warning : rel missing

--- src/components/About.js
+++ src/components/About.js
@@ -33,69 +33,69 @@ function About() {
         {/* LUCIEN */}
         <p><span>Lucien Chotard</span> - Back/Front-end Developper</p>
         <div className="social-container">
-          <a href="https://twitter.com/ChotardLucien" target="_blank"><img className="twitter" src={twitter} alt="twitter"></img></a>
-          <a href="https://www.linkedin.com/in/lucien-chotard/" target="_blank"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
-          <a href="https://github.com/lucienchotard" target="_blank"><img className="github" src={github} alt="github"></img></a>
-          <a href="https://www.behance.net/lucienchotard" target="_blank"><img className="behance" src={behance} alt="behance"></img></a>
-          <a href="https://dribbble.com/lucienchotard" target="_blank"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
-          {/* <a href="" target="_blank"><img className="discord" src={discord} alt="discord"></img></a> */}
+          <a href="https://twitter.com/ChotardLucien" target="_blank"  rel="noopener noreferrer"><img className="twitter" src={twitter} alt="twitter"></img></a>
+          <a href="https://www.linkedin.com/in/lucien-chotard/" target="_blank"  rel="noopener noreferrer"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
+          <a href="https://github.com/lucienchotard" target="_blank"  rel="noopener noreferrer"><img className="github" src={github} alt="github"></img></a>
+          <a href="https://www.behance.net/lucienchotard" target="_blank"  rel="noopener noreferrer"><img className="behance" src={behance} alt="behance"></img></a>
+          <a href="https://dribbble.com/lucienchotard" target="_blank"  rel="noopener noreferrer"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="discord" src={discord} alt="discord"></img></a> */}
         </div>
 
         {/* Matthieu */}
-        <p><a href="http://www.matthieuvidal.com/" target="_blank">Matthieu Vidal</a> - Back-end Developper</p>
+        <p><a href="http://www.matthieuvidal.com/" target="_blank"  rel="noopener noreferrer">Matthieu Vidal</a> - Back-end Developper</p>
         <div className="social-container">
-          {/* <a href="http://www.matthieuvidal.com/" target="_blank"><img className="folio" src={folio} alt="folio"></img></a> */}
-          <a href="https://twitter.com/matthieuvdl" target="_blank"><img className="twitter" src={twitter} alt="twitter"></img></a>
-          <a href="https://www.linkedin.com/in/matthieuvdl/" target="_blank"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
-          <a href="https://github.com/matthieuvdl" target="_blank"><img className="github" src={github} alt="github"></img></a>
-          {/* <a href="" target="_blank"><img className="behance" src={behance} alt="behance"></img></a> */}
-          {/* <a href="" target="_blank"><img className="dribbble" src={dribbble} alt="dribbble"></img></a> */}
-          <a href="https://discord.com/users/161905660735389696" target="_blank"><img className="discord" src={discord} alt="discord"></img></a>
+          {/* <a href="http://www.matthieuvidal.com/" target="_blank"  rel="noopener noreferrer"><img className="folio" src={folio} alt="folio"></img></a> */}
+          <a href="https://twitter.com/matthieuvdl" target="_blank"  rel="noopener noreferrer"><img className="twitter" src={twitter} alt="twitter"></img></a>
+          <a href="https://www.linkedin.com/in/matthieuvdl/" target="_blank"  rel="noopener noreferrer"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
+          <a href="https://github.com/matthieuvdl" target="_blank"  rel="noopener noreferrer"><img className="github" src={github} alt="github"></img></a>
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="behance" src={behance} alt="behance"></img></a> */}
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="dribbble" src={dribbble} alt="dribbble"></img></a> */}
+          <a href="https://discord.com/users/161905660735389696" target="_blank"  rel="noopener noreferrer"><img className="discord" src={discord} alt="discord"></img></a>
         </div>
 
         {/* Cathy */}
-        <p><a href="https://cathydolle.com/" target="_blank">Cathy Dolle</a> - Front-end Developper | UI/UX Designer</p>
+        <p><a href="https://cathydolle.com/" target="_blank"  rel="noopener noreferrer">Cathy Dolle</a> - Front-end Developper | UI/UX Designer</p>
         <div className="social-container">
-          <a href="https://cathydolle.com/" target="_blank"><img className="folio" src={folio} alt="folio"></img></a>
-          <a href="https://twitter.com/cathydolle" target="_blank"><img className="twitter" src={twitter} alt="twitter"></img></a>
-          <a href="https://www.linkedin.com/in/cathy-dolle/" target="_blank"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
-          <a href="https://github.com/CathyDolle" target="_blank"><img className="github" src={github} alt="github"></img></a>
-          <a href="https://www.behance.net/cathydolle" target="_blank"><img className="behance" src={behance} alt="behance"></img></a>
-          <a href="https://dribbble.com/cathydolle" target="_blank"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
-          <a href="https://discord.com/users/169782332247506944" target="_blank"><img className="discord" src={discord} alt="discord"></img></a>
+          <a href="https://cathydolle.com/" target="_blank"  rel="noopener noreferrer"><img className="folio" src={folio} alt="folio"></img></a>
+          <a href="https://twitter.com/cathydolle" target="_blank"  rel="noopener noreferrer"><img className="twitter" src={twitter} alt="twitter"></img></a>
+          <a href="https://www.linkedin.com/in/cathy-dolle/" target="_blank"  rel="noopener noreferrer"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
+          <a href="https://github.com/CathyDolle" target="_blank"  rel="noopener noreferrer"><img className="github" src={github} alt="github"></img></a>
+          <a href="https://www.behance.net/cathydolle" target="_blank"  rel="noopener noreferrer"><img className="behance" src={behance} alt="behance"></img></a>
+          <a href="https://dribbble.com/cathydolle" target="_blank"  rel="noopener noreferrer"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
+          <a href="https://discord.com/users/169782332247506944" target="_blank"  rel="noopener noreferrer"><img className="discord" src={discord} alt="discord"></img></a>
         </div>
 
         {/* Anouk */}
         <p><span>Anouk Hervouet</span> - UI/UX Designer</p>
         <div className="social-container">
-          {/* <a href="" target="_blank"><img className="twitter" src={twitter} alt="twitter"></img></a> */}
-          <a href="https://www.linkedin.com/in/anouk-hervouet-583977186/" target="_blank"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
-          {/* <a href="" target="_blank"><img className="github" src={github} alt="github"></img></a> */}
-          <a href="https://www.behance.net/anoukhervouet" target="_blank"><img className="behance" src={behance} alt="behance"></img></a>
-          <a href="https://dribbble.com/anouk_hervouet" target="_blank"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
-          <a href="https://discord.com/users/558295665734582287" target="_blank"><img className="discord" src={discord} alt="discord"></img></a>
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="twitter" src={twitter} alt="twitter"></img></a> */}
+          <a href="https://www.linkedin.com/in/anouk-hervouet-583977186/" target="_blank"  rel="noopener noreferrer"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="github" src={github} alt="github"></img></a> */}
+          <a href="https://www.behance.net/anoukhervouet" target="_blank"  rel="noopener noreferrer"><img className="behance" src={behance} alt="behance"></img></a>
+          <a href="https://dribbble.com/anouk_hervouet" target="_blank"  rel="noopener noreferrer"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
+          <a href="https://discord.com/users/558295665734582287" target="_blank"  rel="noopener noreferrer"><img className="discord" src={discord} alt="discord"></img></a>
         </div>
 
         {/* Sibylle */}
         <p><span>Sibylle Hervouet</span> - UI/UX Designer</p>
         <div className="social-container">
-          <a href="https://www.twitter.com/sibylle_hrvt" target="_blank"><img className="twitter" src={twitter} alt="twitter"></img></a>
-          <a href="https://www.linkedin.com/in/sibylle-hervouet" target="_blank"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
-          {/* <a href="" target="_blank"><img className="github" src={github} alt="github"></img></a> */}
-          <a href="https://www.behance.net/sibyllehervouet" target="_blank"><img className="behance" src={behance} alt="behance"></img></a>
-          <a href="https://dribbble.com/Sibsib" target="_blank"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
-          <a href="https://discord.com/users/664490938831208482" target="_blank"><img className="discord" src={discord} alt="discord"></img></a>
+          <a href="https://www.twitter.com/sibylle_hrvt" target="_blank"  rel="noopener noreferrer"><img className="twitter" src={twitter} alt="twitter"></img></a>
+          <a href="https://www.linkedin.com/in/sibylle-hervouet" target="_blank"  rel="noopener noreferrer"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="github" src={github} alt="github"></img></a> */}
+          <a href="https://www.behance.net/sibyllehervouet" target="_blank"  rel="noopener noreferrer"><img className="behance" src={behance} alt="behance"></img></a>
+          <a href="https://dribbble.com/Sibsib" target="_blank"  rel="noopener noreferrer"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
+          <a href="https://discord.com/users/664490938831208482" target="_blank"  rel="noopener noreferrer"><img className="discord" src={discord} alt="discord"></img></a>
         </div>
 
         {/* Myriam */}
         <p><span>Myriam Abid Onn</span> - UI/UX Designer | Resp. Marketing</p>
         <div className="social-container">
-          {/* <a href="" target="_blank"><img className="twitter" src={twitter} alt="twitter"></img></a> */}
-          <a href="http://www.linkedin.com/in/myriam-abidonn" target="_blank"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
-          {/* <a href="" target="_blank"><img className="github" src={github} alt="github"></img></a> */}
-          <a href="https://www.behance.net/mabidonn" target="_blank"><img className="behance" src={behance} alt="behance"></img></a>
-          <a href="https://dribbble.com/mabidonn" target="_blank"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
-          <a href="https://discord.com/users/664058855235256353" target="_blank"><img className="discord" src={discord} alt="discord"></img></a> 
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="twitter" src={twitter} alt="twitter"></img></a> */}
+          <a href="http://www.linkedin.com/in/myriam-abidonn" target="_blank"  rel="noopener noreferrer"><img className="linkedin" src={linkedin} alt="linkedin"></img></a>
+          {/* <a href="" target="_blank"  rel="noopener noreferrer"><img className="github" src={github} alt="github"></img></a> */}
+          <a href="https://www.behance.net/mabidonn" target="_blank"  rel="noopener noreferrer"><img className="behance" src={behance} alt="behance"></img></a>
+          <a href="https://dribbble.com/mabidonn" target="_blank"  rel="noopener noreferrer"><img className="dribbble" src={dribbble} alt="dribbble"></img></a>
+          <a href="https://discord.com/users/664058855235256353" target="_blank"  rel="noopener noreferrer"><img className="discord" src={discord} alt="discord"></img></a> 
         </div>
 
       </div>
