c569bd16a4d32fd5ca3c9706172f45b105910a4e
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-06-24 19:34:34 +0200

    Updates

---
+++ precache-manifest.0a7ee8bf0cf8e8658cb4ef2e0ca40140.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601f42f6260c088d0931468ca6258863",
+    "url": "/WorldWideMusic-deploy/index.html"
+  },
+  {
+    "revision": "d8ebceeb2087a50144c5",
+    "url": "/WorldWideMusic-deploy/static/css/main.f0cc0621.chunk.css"
+  },
+  {
+    "revision": "fd3746438c5f34275d78",
+    "url": "/WorldWideMusic-deploy/static/js/3.4fd16054.chunk.js"
+  },
+  {
+    "revision": "32ed91321400209fdad6",
+    "url": "/WorldWideMusic-deploy/static/js/4.74b206cd.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/WorldWideMusic-deploy/static/js/4.74b206cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd6e645e2027e6e55c3",
+    "url": "/WorldWideMusic-deploy/static/js/5.f545843f.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/WorldWideMusic-deploy/static/js/5.f545843f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2c92ec1ce5ec4a5885",
+    "url": "/WorldWideMusic-deploy/static/js/6.69351409.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/WorldWideMusic-deploy/static/js/6.69351409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ebceeb2087a50144c5",
+    "url": "/WorldWideMusic-deploy/static/js/main.99985c67.chunk.js"
+  },
+  {
+    "revision": "a774d946bd4bb7b11467",
+    "url": "/WorldWideMusic-deploy/static/js/runtime-main.05294fe9.js"
+  },
+  {
+    "revision": "70caeb51d916dd67ff63",
+    "url": "/WorldWideMusic-deploy/static/js/xlsx.ae88dbed.chunk.js"
+  },
+  {
+    "revision": "958c16baf2adde22042df91928eb040c",
+    "url": "/WorldWideMusic-deploy/static/media/Europa-Bold.958c16ba.woff"
+  },
+  {
+    "revision": "537053f56d83c24ee307d8d1b98214e5",
+    "url": "/WorldWideMusic-deploy/static/media/Europa-Regular.537053f5.woff2"
+  },
+  {
+    "revision": "9d2df32ed2a7c2fd6a4b191d4228d489",
+    "url": "/WorldWideMusic-deploy/static/media/Europa-Regular.9d2df32e.woff"
+  },
+  {
+    "revision": "f2642cbf231b87a8995eb99241c63d94",
+    "url": "/WorldWideMusic-deploy/static/media/behance.f2642cbf.svg"
+  },
+  {
+    "revision": "0dbe92286a4c3ac01c2111fb26ba9208",
+    "url": "/WorldWideMusic-deploy/static/media/close.0dbe9228.svg"
+  },
+  {
+    "revision": "31b8e5f1f90d9f4bf60d5dfa8e2b3397",
+    "url": "/WorldWideMusic-deploy/static/media/discord.31b8e5f1.svg"
+  },
+  {
+    "revision": "29e61610efdf9d82835d60c6ce6ea58f",
+    "url": "/WorldWideMusic-deploy/static/media/dribbble.29e61610.svg"
+  },
+  {
+    "revision": "6c4b0fccbdcd60b0475edd5fd801a7d4",
+    "url": "/WorldWideMusic-deploy/static/media/eye.6c4b0fcc.svg"
+  },
+  {
+    "revision": "79ac78e7aaace70ca5949671b7e088a5",
+    "url": "/WorldWideMusic-deploy/static/media/eyeclose.79ac78e7.svg"
+  },
+  {
+    "revision": "7812a91c79e079671d3d1996865cb79b",
+    "url": "/WorldWideMusic-deploy/static/media/folio.7812a91c.svg"
+  },
+  {
+    "revision": "99ee9cfd2436c7ed263dbbe902635b58",
+    "url": "/WorldWideMusic-deploy/static/media/github.99ee9cfd.svg"
+  },
+  {
+    "revision": "d5554a9214ac6412f49c69d747ee7fcd",
+    "url": "/WorldWideMusic-deploy/static/media/globe.d5554a92.svg"
+  },
+  {
+    "revision": "a6b021d6d47983cda8638121cbad782d",
+    "url": "/WorldWideMusic-deploy/static/media/linkedin.a6b021d6.svg"
+  },
+  {
+    "revision": "b8e2ba72da0d2beeef03b23f30c2522b",
+    "url": "/WorldWideMusic-deploy/static/media/logo.b8e2ba72.svg"
+  },
+  {
+    "revision": "6b42ed80b85407b7c15c3af37ee0ebc9",
+    "url": "/WorldWideMusic-deploy/static/media/map.6b42ed80.svg"
+  },
+  {
+    "revision": "fa0c36a0d3668d57046822a1d97ec227",
+    "url": "/WorldWideMusic-deploy/static/media/mapIcon.fa0c36a0.svg"
+  },
+  {
+    "revision": "1e6d8f6eb12de61b77487785f21b8af2",
+    "url": "/WorldWideMusic-deploy/static/media/poster.1e6d8f6e.svg"
+  },
+  {
+    "revision": "908e8036cda03ca9707a8602a9289d03",
+    "url": "/WorldWideMusic-deploy/static/media/productAlbums.908e8036.svg"
+  },
+  {
+    "revision": "be3e6e33223b297f1db709e4f64a41e4",
+    "url": "/WorldWideMusic-deploy/static/media/twitter.be3e6e33.svg"
+  },
+  {
+    "revision": "7064dd423bc4829f6b452e882468249f",
+    "url": "/WorldWideMusic-deploy/static/media/video.7064dd42.mp4"
+  },
+  {
+    "revision": "37b4b24755fa1288747fca0473ab9fa1",
+    "url": "/WorldWideMusic-deploy/static/media/wave.37b4b247.svg"
+  }
+]);
---
+++ static/js/xlsx.ae88dbed.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmap=this.webpackJsonpmap||[]).push([[2],{307:function(n,p){},397:function(n,p){},398:function(n,p){}}]);
+//# sourceMappingURL=xlsx.ae88dbed.chunk.js.map
