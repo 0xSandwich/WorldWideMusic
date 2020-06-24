cac0b39bcf30ddbe4e0d0aefccff7872b50525d8
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-06-24 20:17:37 +0200

    Updates

---
+++ precache-manifest.be5029757343b9d827de42197d87e1f6.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a3ec5f1807a2d9f3a9ddcb8c441dc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123577f6724430cfb52f",
+    "url": "/static/css/main.3552608a.chunk.css"
+  },
+  {
+    "revision": "fd3746438c5f34275d78",
+    "url": "/static/js/3.4fd16054.chunk.js"
+  },
+  {
+    "revision": "32ed91321400209fdad6",
+    "url": "/static/js/4.74b206cd.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/static/js/4.74b206cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd6e645e2027e6e55c3",
+    "url": "/static/js/5.f545843f.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.f545843f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2c92ec1ce5ec4a5885",
+    "url": "/static/js/6.69351409.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/6.69351409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123577f6724430cfb52f",
+    "url": "/static/js/main.9733a496.chunk.js"
+  },
+  {
+    "revision": "c16e0f8af57cf15d8957",
+    "url": "/static/js/runtime-main.d29d630f.js"
+  },
+  {
+    "revision": "70caeb51d916dd67ff63",
+    "url": "/static/js/xlsx.ae88dbed.chunk.js"
+  },
+  {
+    "revision": "958c16baf2adde22042df91928eb040c",
+    "url": "/static/media/Europa-Bold.958c16ba.woff"
+  },
+  {
+    "revision": "537053f56d83c24ee307d8d1b98214e5",
+    "url": "/static/media/Europa-Regular.537053f5.woff2"
+  },
+  {
+    "revision": "9d2df32ed2a7c2fd6a4b191d4228d489",
+    "url": "/static/media/Europa-Regular.9d2df32e.woff"
+  },
+  {
+    "revision": "f2642cbf231b87a8995eb99241c63d94",
+    "url": "/static/media/behance.f2642cbf.svg"
+  },
+  {
+    "revision": "0dbe92286a4c3ac01c2111fb26ba9208",
+    "url": "/static/media/close.0dbe9228.svg"
+  },
+  {
+    "revision": "31b8e5f1f90d9f4bf60d5dfa8e2b3397",
+    "url": "/static/media/discord.31b8e5f1.svg"
+  },
+  {
+    "revision": "29e61610efdf9d82835d60c6ce6ea58f",
+    "url": "/static/media/dribbble.29e61610.svg"
+  },
+  {
+    "revision": "6c4b0fccbdcd60b0475edd5fd801a7d4",
+    "url": "/static/media/eye.6c4b0fcc.svg"
+  },
+  {
+    "revision": "79ac78e7aaace70ca5949671b7e088a5",
+    "url": "/static/media/eyeclose.79ac78e7.svg"
+  },
+  {
+    "revision": "7812a91c79e079671d3d1996865cb79b",
+    "url": "/static/media/folio.7812a91c.svg"
+  },
+  {
+    "revision": "99ee9cfd2436c7ed263dbbe902635b58",
+    "url": "/static/media/github.99ee9cfd.svg"
+  },
+  {
+    "revision": "d5554a9214ac6412f49c69d747ee7fcd",
+    "url": "/static/media/globe.d5554a92.svg"
+  },
+  {
+    "revision": "a6b021d6d47983cda8638121cbad782d",
+    "url": "/static/media/linkedin.a6b021d6.svg"
+  },
+  {
+    "revision": "b8e2ba72da0d2beeef03b23f30c2522b",
+    "url": "/static/media/logo.b8e2ba72.svg"
+  },
+  {
+    "revision": "6b42ed80b85407b7c15c3af37ee0ebc9",
+    "url": "/static/media/map.6b42ed80.svg"
+  },
+  {
+    "revision": "fa0c36a0d3668d57046822a1d97ec227",
+    "url": "/static/media/mapIcon.fa0c36a0.svg"
+  },
+  {
+    "revision": "1e6d8f6eb12de61b77487785f21b8af2",
+    "url": "/static/media/poster.1e6d8f6e.svg"
+  },
+  {
+    "revision": "908e8036cda03ca9707a8602a9289d03",
+    "url": "/static/media/productAlbums.908e8036.svg"
+  },
+  {
+    "revision": "be3e6e33223b297f1db709e4f64a41e4",
+    "url": "/static/media/twitter.be3e6e33.svg"
+  },
+  {
+    "revision": "7064dd423bc4829f6b452e882468249f",
+    "url": "/static/media/video.7064dd42.mp4"
+  },
+  {
+    "revision": "37b4b24755fa1288747fca0473ab9fa1",
+    "url": "/static/media/wave.37b4b247.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WorldWideMusic/precache-manifest.e9f28c862a509598ce670e799c857577.js"
+  "/precache-manifest.be5029757343b9d827de42197d87e1f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WorldWideMusic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
