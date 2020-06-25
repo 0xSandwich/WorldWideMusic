57c1d513ee44c3005cf918d2ca5763a03aec773d
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-06-25 09:29:19 +0200

    Fix decade bar 90's

--- src/components/DecadeInput.js
+++ src/components/DecadeInput.js
@@ -12,7 +12,7 @@ export class decadeInput extends Component {
                     <li className={this.props.curdecade === 1960 ? "active" : null} onClick={()=>{this.props.handleNav(1960)}}>1960's</li>
                     <li className={this.props.curdecade === 1970 ? "active" : null} onClick={()=>{this.props.handleNav(1970)}}>1970's</li>
                     <li className={this.props.curdecade === 1980 ? "active" : null} onClick={()=>{this.props.handleNav(1980)}}>1980's</li>
-                    <li className={this.props.curdecade === 1990 ? "active" : null} onClick={()=>{this.props.handleNav(1990)}}>1990's</li>
+                    <li className={this.props.curdecade == 1990 ? "active" : null} onClick={()=>{this.props.handleNav(1990)}}>1990's</li>
                     <li className={this.props.curdecade === 2000 ? "active" : null} onClick={()=>{this.props.handleNav(2000)}}>2000's</li>
                     <li className={this.props.curdecade === 2010 ? "active" : null} onClick={()=>{this.props.handleNav(2010)}}>2010's</li>
                     <li className={this.props.curdecade === 2020 ? "active" : null} onClick={()=>{this.props.handleNav(2020)}}>2020's</li>
